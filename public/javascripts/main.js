f7fd06dde90351012e38bd1bedd6b22618c95a79
Frank Quednau <fquednau@gmail.com>
2014-07-09 23:28:50 +0200

    New version with vis that works

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,20 +19,43 @@ function asStream(textfield) {
         .distinctUntilChanged();
 };
 
+function packageToNet(package) {
+
+  function hashCode(str) {
+    var hash = 0, i, chr, len;
+    if (str.length == 0) return hash;
+    for (i = 0, len = str.length; i < len; i++) {
+      chr   = str.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
+  
+  var root = {
+    id : hashCode(package.Id),
+    label : package.Id + "(" + package.Version + ")"
+  };
+
+  var childNodes = _.map(package.Dependencies.split("|"), function(text) {
+      return { id : hashCode(text), label : text };
+  });
+
+  var edges = _.map(childNodes, function(cn) {
+    return { from: root.id, to: cn.id };
+  });
+
+  return {
+    nodes: [root].concat(childNodes),
+    edges: edges
+  };
+}
+
+
 function fxNuget(results, packageLoadCallback) {
   var url = Handlebars.compile("http://www.nuget.org/api/v2/Search()?$filter=IsLatestVersion&$skip=0&$top=10&searchTerm='{{this}}'&includePrerelease=false")
   var singlePackage = Handlebars.compile("http://www.nuget.org/api/v2/Packages()?$filter=Id%20eq%20%27{{this.Id}}%27%20and%20Version%20eq%20%27{{this.Version}}%27")
   
-  var tuneJson = function(package) {
-    return {
-      id : package.Id,
-      name : package.Id + "(" + package.Version + ")",
-      data : {},
-      children : _.map(package.Dependencies.split("|"), function(text) {
-        return { id : text, name : text, data: {}, children: [] };
-      })
-    }
-  }
 
   var loadPackage = function(item) {
       OData.read(
@@ -42,7 +65,7 @@ function fxNuget(results, packageLoadCallback) {
       },
       function (data,response) {
         var package = data.results[0];
-        packageLoadCallback(tuneJson(package));
+        packageLoadCallback(packageToNet(package));
       });
     };
 
