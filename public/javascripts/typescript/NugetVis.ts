281360f9dea258f039103fb709d234180b877457
Frank Quednau <fquednau@gmail.com>
2014-08-03 22:39:45 +0200

    Interim checking doing the package loading for the graph

--- public/javascripts/typescript/NugetVis.ts
+++ public/javascripts/typescript/NugetVis.ts
@@ -4,9 +4,12 @@ module NugetVis {
 
 	declare var OData;
 
-	export interface NugetPackage {
+	export interface VersionedPackage {
 		Id : string;
 		Version : string;
+	}
+
+	export interface NugetPackage extends VersionedPackage {
 		Description : string;
 	}
 
@@ -14,15 +17,14 @@ module NugetVis {
 		Dependencies : string;
 	}
 
-	export interface Dependency {
-		Id : string;
-		Version : string;
+	export interface Dependency extends VersionedPackage {
 		TargetFw : string;
 	}
 
 	export interface LocalPackage extends NugetPackage {
 		Dependencies : Dependency[];
-		loadPackage : (package : LocalPackage) => void
+		loadPackage : (package : LocalPackage) => void;
+		isFirst? : boolean;
 	}	
 
 
@@ -30,11 +32,14 @@ module NugetVis {
 
 		private searchUrlStr = "/Search()?$filter=IsLatestVersion&$skip=0&$top=10&searchTerm='{term}'&includePrerelease=false";
 		private loadByIdAndVersion = "/Packages()?$filter=Id%20eq%20%27{id}%27%20and%20Version%20eq%20%27{version}%27";
+		private loadByIdOnly =  "/Packages()?$filter=Id%20eq%20%27{id}%27";
 		
-		rootUrl : string;
+		private rootUrl : string;
+		private graphSubject : Rx.Subject<LocalPackage>;
 
 		constructor(theRootUrl : string) {
 			this.rootUrl = theRootUrl;
+			this.graphSubject = new Rx.Subject<LocalPackage>();
 		}
 
 		searchPackages(observableText : Rx.Observable<string>) : Rx.Observable<LocalPackage[]>  {
@@ -45,32 +50,40 @@ module NugetVis {
 						requestUri : this.searchUrl(nextText),
 						enableJsonpCallback : true
 					}, (data, response) => {
-						obs.onNext(this.convertRemoteToLocal(data.results));
+						obs.onNext(this.convertRemotesToLocals(data.results));
 					});
 				});
 				return dispose;
 			});
 		}
 
-		private searchUrl(searchTerm : string) : string {
-			return this.rootUrl + this.searchUrlStr.replace("{term}", searchTerm);
+		graphFillingStream() : Rx.Observable<LocalPackage> {
+			return this.graphSubject;
 		}
 
-		private loadUrl(id : string, version : string) : string {
-			return this.rootUrl + this.loadByIdAndVersion.replace("{id}", id).replace("{version}", version);
+		private loadPackageDeep(package : LocalPackage) {
+			OData.read({
+				requestUri : this.loadUrl(package),
+				enableJsonpCallback : true
+			}, (data, response) => {
+				var package = this.convertRemoteToLocal(data.results[0]);
+				this.graphSubject.onNext(package);
+				//TODO: Keep onloading dependencies, thing with isFirst and stuff.
+			});
 		}
 
-		private convertRemoteToLocal(packages : RemotePackage[]) : LocalPackage [] {
-			return _.map(packages, p => 
-			{
-				return {
+		private convertRemotesToLocals(packages : RemotePackage[]) : LocalPackage [] {
+			return _.map(packages, this.convertRemoteToLocal);
+		}
+
+		private convertRemoteToLocal(p : RemotePackage) : LocalPackage {
+			return {
 					Id : p.Id,
 					Version : p.Version,
 					Description : p.Description,
 					Dependencies: this.constructDependencies(p.Dependencies),
-					loadPackage : p => {} //TODO: Fill with life
+					loadPackage : this.loadPackageDeep
 				};
-			});
 		}
 
 		private constructDependencies(dependencies : string) : Dependency[] {
@@ -93,5 +106,18 @@ module NugetVis {
 			var match = r.exec(versionRange)
 			return match != null ? match[0] : "";
 		}
+
+		private searchUrl(searchTerm : string) : string {
+			return this.rootUrl + this.searchUrlStr.replace("{term}", searchTerm);
+		}
+
+		private loadUrl(package : VersionedPackage) : string {
+			return this.rootUrl + this.loadByIdAndVersion.replace("{id}", package.Id).replace("{version}", package.Version);
+		}
+
+		private loadUrlWithoutVersion(id : string) : string {
+			return this.rootUrl + this.loadByIdOnly.replace("{id}", id);
+		}
+
 	}
 }
