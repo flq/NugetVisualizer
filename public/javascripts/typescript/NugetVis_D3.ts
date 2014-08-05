0467390eb50a9339abaa13c48a4ad6a5f8ad3d67
Frank Quednau <fquednau@gmail.com>
2014-08-05 22:27:19 +0200

    Getting resources for D3 spin off

---
+++ public/javascripts/typescript/NugetVis_D3.ts
@@ -0,0 +1,7 @@
+/// <reference path="NugetVis.ts" />
+/// <reference path="definitelyTyped/rx.js.TypeScript.DefinitelyTyped.0.4.0/Content/Scripts/typings/rx.js/rx.lite.d.ts" />
+/// <reference path="definitelyTyped/underscore.TypeScript.DefinitelyTyped.0.4.4/Content/Scripts/typings/underscore/underscore.d.ts" />
+/// <reference path="definitelyTyped/d3.TypeScript.DefinitelyTyped.1.4.5/Content/Scripts/typings/d3/d3.d.ts" />
+module NugetVis {
+
+}
